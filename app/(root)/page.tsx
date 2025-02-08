b9383fa7eaf2176b7f2bd5c421ab7f001e6521ea
Vivek Kumar <vivekgenius8287@gmail.com>
2025-02-08 14:18:05 +0530

    database connected

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,9 +9,28 @@ export default function Home() {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Event Hosting Or Searching Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">
+              A platform to host, search, manage, view events{" "}
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Find Events</Link>
+            </Button>
           </div>
-
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
         </div>
+      </section>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+      <h2 className="h2-bold">Host Events <br/> As You Always Wanted</h2>
+      <div className="flex w-full flex-col gap-5 md:flex-row">
+        Search
+        CategoryFilter
+      </div>
       </section>
     </>
   );
